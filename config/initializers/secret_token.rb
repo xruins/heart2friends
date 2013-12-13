3fa7da4610921452ca52ecfddeb2c3b1b1d18862
Ruins <ruins@ruins.mobi>
2013-12-13 15:15:21 +0900

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Heart2friends::Application.config.secret_key_base = 'd947467e744dd446f0cde9eeadcf3dad0b87e477e368e600793d5180ab4bfb3a88d0a1633163bef07c8fb93cda7d9a1a60c7a1fc7ab8bf291708cb197b035b7c'
